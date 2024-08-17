0ea2a8b5282c7242d9397a5d0ea9afd38bf6dcf9
gabealoi <gabe.n.aloi@gmail.com>
2024-08-17 17:42:32 -0500

    modified ig content loader to accomodate generation of session from cookies if necessary

--- instagram_content_loader.py
+++ instagram_content_loader.py
@@ -1,21 +1,26 @@
 import instaloader
 import requests
 import os
+import time
 
+from generate_session_from_cookie import generate_session_from_cookies
 from tgram_bot_logger import write_log
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 path_to_session = os.getenv('SESSION_PATH')
 
 def get_instagram_post_media(shortcode):
+
+    attempts = 0
+
     # Initialize Instaloader
     loader = instaloader.Instaloader()
 
     # if we're in the right environment, then load the session; else we good
     if path_to_session:
-        loader.load_session_from_file(username='gabealoi', filename=path_to_session)
+        loader.load_session_from_file(username='tgrambotlord', filename=path_to_session)
         write_log(message=f"Successfully loaded IG session from file", level='info')
     else:
         write_log(message=f"No IG session path found in environment.. Continuing to complete request with instaloader..", level='warning')
@@ -33,11 +38,23 @@ def get_instagram_post_media(shortcode):
         else:
             media_url = post.url
 
-        # download media content
-        response = requests.get(media_url, stream=True)
+        while attempts < 3:
+            write_log(message=f"Make GraphQL Request to Instagram Resources with Instaloader. Attempt {attempts+1} of 3..", level='info')
+            # download media content
+            response = requests.get(media_url, stream=True)
 
-        # response = requests.get(media_url)
-        response.raise_for_status() # ensure we notice bad response
+            # if IG is blocking our request for some reason, we may need to regenerate an IG loader session on-the-fly from a quick login
+            if response.status_code == 401:
+                write_log(message=f"Response Status Code: 401 UNAUTHORIZED. Attempting to regenerate IG Loader Session from cookies on-the-fly..", level='warning')
+                generate_session_from_cookies()
+            else:
+                break
+            
+            attempts += 1
+            time.sleep(5)
+
+
+        response.raise_for_status() # ensure we notice bad response that isn't unauthorized
 
         # store media content in a variable
         media_content = response.content
