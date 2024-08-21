c8d0df59c3a6402e03eeb1d8c74883e152cbc9dd
gabealoi <gabe.n.aloi@gmail.com>
2024-08-21 18:19:02 -0500

    remodeled thread safe version

--- instagram_content_loader.py
+++ instagram_content_loader.py
@@ -12,11 +12,10 @@ path_to_session = os.getenv('SESSION_PATH')
 
 # Create an Event to coordinate the session generation
 session_generation_event = threading.Event()
+session_generation_lock = threading.Lock()
 session_generation_in_progress = False
 
 def get_instagram_post_media(shortcode):
-    global session_generation_in_progress
-
     # Initialize Instaloader
     loader = instaloader.Instaloader()
     # if we're in the right environment, then load the session; else we good
@@ -52,7 +51,13 @@ def get_instagram_post_media(shortcode):
         return None, None, None, None, None, None
 
 def fetch_post_data(loader: Instaloader, shortcode: str, max_attempts: int = 3):
-    global session_generation_event, session_generation_in_progress
+    '''
+        #### Grabs the post data from the Instagram Post API
+
+        Returns the Post object if successful, otherwise raises an Exception.
+        
+        Note: Will attempt to relogin if there are any permissions issues with stale session
+    '''
 
     attempts = 0
     reload_session = False
@@ -60,34 +65,35 @@ def fetch_post_data(loader: Instaloader, shortcode: str, max_attempts: int = 3):
     while attempts < max_attempts:
         try:
             if reload_session:
-                # Ensure session generation happens only once
-                if not session_generation_in_progress:
-                    session_generation_in_progress = True
-                    session_generation_event.clear()
-                    threading.Thread(target=generate_session_wrapper).start()
-                    session_generation_event.wait()
-                    session_generation_in_progress = False
-
                 loader.load_session_from_file(username='tgrambotlord', filename=path_to_session)
                 write_log(message="New session successfully loaded from file", level='info')
-
             return instaloader.Post.from_shortcode(loader.context, shortcode)
-
+        
         except ConnectionException as cex:
             if '401' in str(cex):
                 write_log(message="Response Status Code: 401 UNAUTHORIZED. Attempting to regenerate IG Loader Session from cookies on-the-fly..", level='warning')
-                generate_session_wrapper()  # In case session needs regeneration
-                reload_session = True
+                regenerate_session()
+                reload_session = True  # Reload session next time around
                 attempts += 1
-                time.sleep(5)
+                time.sleep(5)  # Wait before retrying
             else:
                 raise cex
 
     raise Exception(f"Failed to fetch post data after {max_attempts} attempts.")
 
+def regenerate_session():
+    global session_generation_in_progress
+
+    with session_generation_lock:
+        if not session_generation_in_progress:
+            session_generation_in_progress = True
+            session_generation_event.clear()
+            threading.Thread(target=generate_session_wrapper).start()
+            session_generation_event.wait()  # Wait for session generation to complete
+            session_generation_in_progress = False
+
 def generate_session_wrapper():
     try:
         generate_session_from_cookies()
     finally:
-        # Signal that session generation is complete
         session_generation_event.set()
