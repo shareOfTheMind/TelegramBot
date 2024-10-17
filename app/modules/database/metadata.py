baf9966788cdefe6006c41c627bc250f23851471
SlidewhistleIncognito <a.slidewhistle@gmail.com>
2024-10-16 20:04:35 -0400

    update database schema

--- app/modules/database/metadata.py
+++ app/modules/database/metadata.py
@@ -9,7 +9,7 @@ class User(Base):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(32), unique=True)
+    username: Mapped[str] = mapped_column(String(32), unique=True)
 
     posts: Mapped[List["Post"]] = relationship(back_populates="submitter")
 
@@ -24,9 +24,9 @@ class Post(Base):
     poster: Mapped[str] = mapped_column(String(32))
     likes: Mapped[int]
     views: Mapped[int]
-    share_link: Mapped[str] = mapped_column(unique=True)
-    s3_link: Mapped[str] = mapped_column(unique=True)
-    is_video: Mapped[bool]
+    source: Mapped[str]
+    share_link: Mapped[str]
+    file_type: Mapped[str]
 
     submitter_id = mapped_column(ForeignKey("user.id"))
     submitter: Mapped[User] = relationship(back_populates="posts")
