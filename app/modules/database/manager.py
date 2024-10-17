a1390a80ba8d8e515a8b3bce78d46e4c641cadac
gabealoi <gabe.n.aloi@gmail.com>
2024-10-17 10:52:27 -0500

    fixing another syntax error; dumb dumb

--- app/modules/database/manager.py
+++ app/modules/database/manager.py
@@ -16,7 +16,7 @@ class SingletonDBManager(type):
     _lock = threading.Lock()
 
     def __call__(cls, *args, **kwargs):
-        with cls._lock(): # lock to ensure thread safety
+        with cls._lock: # lock to ensure thread safety
             if cls not in cls._instances:
                 cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
